02d6d0842718398d563de6a7cc24bc5ead0f237a
Ícaro Motta <icaromotta@unifei.edu.br>
2023-09-29 14:15:27 -0300

    Atualizando entidade do usuário.

--- entities/user.go
+++ entities/user.go
@@ -7,10 +7,10 @@ import (
 
 type User struct {
 	Id        uuid.UUID `gorm:"->; type: UUID DEFAULT gen_random_uuid(); not null; unique; primaryKey"`
-	Name      string    `gorm:"<-; type: VARCHAR (50); not null"`
-	Email     string    `gorm:"<-; type: VARCHAR (50); not null; unique"`
-	Birthdate time.Time
-	Role      string
-	CreatedAt time.Time `gorm:"<-create; index"`
+	Name      string    `gorm:"type: VARCHAR(50); not null; index"`
+	Email     string    `gorm:"type: VARCHAR(50); not null; unique"`
+	Birthdate time.Time `gorm:"not null"`
+	Role      string    `gorm:"type: VARCHAR(15); not null"`
+	CreatedAt time.Time `gorm:"->; type: timestamp with time zone DEFAULT NOW(); index"`
 	UpdatedAt time.Time
 }
